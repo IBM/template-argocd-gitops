e7ed789cea34f6c51855a7551e2c20b24e234f68
Sean Sundberg <seansund@us.ibm.com>
2020-11-17 09:49:21 -0600

    Adds script to configure credentials for a git org

---
+++ setup/3-add-org-credentials.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+GIT_REPO="$1"
+if [[ -z "${GIT_REPO}" ]]; then
+  GIT_REPO=$(git remote get-url origin | sed -E "s~(.*)/.*~\1~g")
+fi
+
+if [[ "${GIT_REPO}" =~ ^git ]]; then
+  echo -n "Provide the path to the private SSH key: "
+  read KEY_PATH
+
+  argocd repocreds add "${GIT_REPO}" \
+    --ssh-private-key-path "${KEY_PATH}"
+else
+  echo -n "Username for git repo (${GIT_REPO}): "
+  read GIT_USER
+
+  echo -n "Personal access token for git repo (${GIT_REPO}): "
+  read -p GIT_PASSWORD
+
+  argocd repocreds add "${GIT_REPO}" \
+    --username "${GIT_USER}" \
+    --password "${GIT_PASSWORD}"
+fi
