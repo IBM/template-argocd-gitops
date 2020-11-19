06279f74f4140fc582de420626f28dc0e413e04d
Sean Sundberg <seansund@us.ibm.com>
2020-11-19 11:37:50 -0600

    Fixes bug in repo generation and adds auto sync flag

--- setup/4-argocd-bootstrap.sh
+++ setup/4-argocd-bootstrap.sh
@@ -13,15 +13,20 @@ fi
 
 GIT_REPO=$(git remote get-url origin)
 if [[ "${GIT_REPO}" =~ ^git ]]; then
-  GIT_REPO=$(echo "${GIT_REPO}" | sed "s~git@~https://~g" | sed "s~:~/~g")
+  GIT_REPO=$(echo "${GIT_REPO}" | sed "s~git@~https://~g" | sed "s~:~/~g" | sed "s/https\//https:/g")
 fi
+
+echo "Git repo: $GIT_REPO"
 
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 argocd app create "${BOOTSTRAP_PATH}" \
   --repo "${GIT_REPO}" \
-  --revision "${GIT_BRANCH}"
+  --revision "${GIT_BRANCH}" \
   --path "${BOOTSTRAP_PATH}" \
   --dest-namespace $(oc project -q) \
   --dest-server https://kubernetes.default.svc \
-  --project default
+  --project default \
+  --sync-policy automated \
+  --self-heal \
+  --sync-option Prune=true
